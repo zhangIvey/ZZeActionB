55315a859e7b94a0a09194932ecd0d71aba5dae8
zhangze <zhangze@wanbu.com.cn>
2017-10-27 13:51:00 +0800

    0.0.7

---
+++ ZZeActionB/ZZeActionB/Bussness/C_model.h
@@ -0,0 +1,13 @@
+//
+//  C_model.h
+//  ZZeActionB
+//
+//  Created by yaoln on 2017/10/27.
+//  Copyright © 2017年 zz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface C_model : NSObject
+
+@end
---
+++ ZZeActionB/ZZeActionB/Bussness/C_model.m
@@ -0,0 +1,13 @@
+//
+//  C_model.m
+//  ZZeActionB
+//
+//  Created by yaoln on 2017/10/27.
+//  Copyright © 2017年 zz. All rights reserved.
+//
+
+#import "C_model.h"
+
+@implementation C_model
+
+@end
